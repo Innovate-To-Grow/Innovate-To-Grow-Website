e944c976027b77cfd8be67d3c8062189466b43c3
Alvin To <alvin.toto258@gmail.com>
2022-11-06 14:50:58 -0800

    idk bro

--- test.py
+++ test.py
@@ -3,9 +3,3 @@ from werkzeug.datastructures import MultiDict
 from multiprocessing import Process
 from project import wks
 from gspread import cell
-
-s = "i like asian girls"
-a = s.split()
-a.insert(0, "")
-
-print(a)
