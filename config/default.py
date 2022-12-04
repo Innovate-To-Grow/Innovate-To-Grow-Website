77a399c8acd12b3258f1d726d7007cefd95623d4
Alvin To <alvin.toto258@gmail.com>
2022-12-04 01:40:07 -0800

    some config changes

--- config/default.py
+++ config/default.py
@@ -7,8 +7,8 @@ APP_ROOT = re.sub('config', '', APP_ROOT) + "/project"
 
 
 class Config():
-    SECRET_KEY = "98226"
-    SECURITY_PASSWORD_SALT = "38159"
+    SECRET_KEY = "\xa6NF\x17\x8b\xc7a\xcc\x80`\xef\x90\x13M\xcc\xe5\xa8\x05\xed\x07\n\xa5oN"
+    SECURITY_PASSWORD_SALT = "\xb6\x04\x91\xf8\xcf\x02CKT\xc6G\xef\x9fq\xe0\xff\xbfu\xd4\x10q\x07\x8a"
 
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + APP_ROOT + "/db/data.sqlite3"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -16,9 +16,9 @@ class Config():
     MAIL_SERVER = "smtp.gmail.com"
     IMAP_SERVER = "imap.gmail.com"
 
-    MAIL_ALIAS = "Innovate to Grow - UC Merced"
     MAIL_USERNAME = "i2g@g.ucmerced.edu"
     MAIL_PASSWORD = "iekmvhditchuxnik"
+    MAIL_ALIAS = "Innovate to Grow - UC Merced"
 
     URL_PREFIX = "/membership"
 
