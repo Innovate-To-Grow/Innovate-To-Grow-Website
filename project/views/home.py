d0c3ea1e170e4cd6ac5b361e00a12e7d005c3901
martinurueta <uruetaiiimartin@gmail.com>
2022-12-20 14:49:58 -0800

    portion of the webpage pushed

--- project/views/home.py
+++ project/views/home.py
@@ -11,9 +11,21 @@ def page_not_found(e):
 
 @home_blueprint.route("/", methods=["GET", "POST"])
 def mainpage():
-    return render_template("test.html")
+    return render_template("index.html")
 
 
-@home_blueprint.route("/test", methods=["GET", "POST"])
-def mainpage2():
-    return render_template("test2.html")
+@home_blueprint.route("/about", methods=["GET", "POST"])
+def about():
+    return render_template("about.html")
+
+@home_blueprint.route("/engineering-capstone", methods=["GET", "POST"])
+def engineering_capstone():
+    return render_template("engineering-capstone.html")
+
+@home_blueprint.route("/about_EngSL", methods=["GET", "POST"])
+def about_EngSL():
+    return render_template("about_EngSL.html")
+
+@home_blueprint.route("/software-capstone", methods=["GET", "POST"])
+def software_capstone():
+    return render_template("software-capstone.html")
