3b9730a2ed602ccbc4b02666f02ad43b25468a9f
Cody <hotcococody@gmail.com>
2024-05-15 05:14:10 -0700

    updated winners table to homepage

--- project/views/home.py
+++ project/views/home.py
@@ -10,9 +10,9 @@ home_blueprint = Blueprint("home", __name__, template_folder="../templates/home"
 @home_blueprint.route("/", methods=["GET", "POST"])
 @cache.cached()
 def mainpage():
-    return render_template("home-pre-event.html")
+    # return render_template("home-pre-event.html")
     # return render_template("home-during-event.html")
-    # return render_template("home-post-event.html")
+    return render_template("home-post-event.html")
 
 @home_blueprint.route("/about", methods=["GET", "POST"])
 @cache.cached()
