f2d369b8933c57f5b1f6d2fdabedd48bddef9782
Seth <142448427+Flucoreo@users.noreply.github.com>
2025-01-31 19:34:35 -0800

    Updated website to home-during-semester

--- project/views/home.py
+++ project/views/home.py
@@ -12,8 +12,8 @@ home_blueprint = Blueprint("home", __name__, template_folder="../templates/home"
 def mainpage():
     # return render_template("home-pre-event.html")
     # return render_template("home-during-event.html")
-    return render_template("home-post-event.html")
-    # return render_template("home-during-semester.html")
+    # return render_template("home-post-event.html")
+    return render_template("home-during-semester.html")
 
 @home_blueprint.route("/about", methods=["GET", "POST"])
 @cache.cached()
