41b1b6c0e24da659e26b262855fad02a3f14f380
Novix609 <novixstarseeker@gmail.com>
2025-03-20 18:47:04 -0700

    David G's changes to make profile page much friendlier

--- wsgi.py
+++ wsgi.py
@@ -23,15 +23,4 @@ def run_flask():
     app.run(port=5000, debug=True)
 
 if __name__ == "__main__":
-    # Create processes instead of threads
-    fastapi_process = multiprocessing.Process(target=run_fastapi)
-    flask_process = multiprocessing.Process(target=run_flask)
-    
-    # Start both processes
-    fastapi_process.start()
-    flask_process.start()
-    
-    # Wait for both processes to complete
-    fastapi_process.join()
-    flask_process.join()
-    # app.run()
+    app.run()
