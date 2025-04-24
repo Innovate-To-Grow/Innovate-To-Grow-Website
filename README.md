cfa2c7669f8923b0d27c5229f13fea63359ad096
mpkinja <mpkinja@gmail.com>
2025-04-24 16:53:09 -0700

    Revert "Merge pull request #56 from Innovate-To-Grow/team-325-2025-DavidG"
    This reverts commit d7a00f14a7f825e2b7f5f5799c5781bb82a8c8ca, reversing
    changes made to 5697f33bfb3146f19b13922667fb1747a0a0d18d.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-![Use Case Diagram](https://github.com/user-attachments/assets/d91ca946-848b-445f-aa7d-d79ec539c49a)
+# i2g-double-mail
--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-# from flask import Flask, render_template, redirect, url_for, request
 from project import app
 from waitress import serve
-
-# app = Flask(__name__,)
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=5000, threads=8)
